e0485ced91ea5963c3dbaec96a0d86f8eb035839
Emrah <emrah@skyneb.com>
2022-09-22 14:43:39 +0300

    react entegrated to nft mint dapp

---
+++ src/App.js
@@ -0,0 +1,171 @@
+import nftsGif from './preview.gif';
+import './App.css';
+import { useState, useEffect } from "react";
+import { 
+  connectWallet, 
+  getCurrentWalletConnected, 
+  web3, 
+  getMaxMintAmount,
+  getTotalSupply,
+  getNftPrice,
+  mintNFT,
+  getSaleState, } from "./interact";
+import { chainMap } from "./chains";
+
+
+const App = () => {
+
+const [status, setStatus]  = useState("");
+const [walletAddress, setWalletAddress] = useState("");
+const [walletBalance, setWalletBalance] = useState(0);
+const [walletChainName, setWalletChainName] = useState(0);
+const [count, setCount] = useState(1);
+const [maxMintAmount, setMaxMintAmount] = useState(0);
+const [totalSupply, setTotalSupply] = useState(0);
+const [nftPrice, setNftPrice] = useState("0.01");
+const [isSaleActive, setIsSaleActive] = useState(true);
+
+const connectWalletPressed = async () => {
+  const walletResponse = await connectWallet();
+  setWalletAddress(walletResponse.address);
+  setStatus(walletResponse.status);
+};
+
+const updateTotalSupply = async () => {
+  const mintedCount = await getTotalSupply();
+  setTotalSupply(mintedCount);
+};
+
+const incrementCount = () => {
+  if (count < maxMintAmount) {
+    setCount(count + 1);
+  }
+};
+
+const decrementCount = () => {
+  if (count > 1) {
+    setCount(count - 1);
+  }
+};
+
+const mintNFTPressed = async () => {
+    const { status } = await mintNFT(count);
+    setStatus(status);
+  
+    // We minted a new token, so we need to update the total supply
+    updateTotalSupply();
+};
+
+
+useEffect(() => {
+  const prepare = async () => {
+    const walletResponse = await getCurrentWalletConnected();
+    setWalletAddress(walletResponse.address);
+    setStatus(walletResponse.status);
+    setWalletChainName(walletResponse.chainName);
+    setWalletBalance(walletResponse.balance);
+
+    setMaxMintAmount(await getMaxMintAmount());
+    setNftPrice(await getNftPrice());
+    setIsSaleActive(await getSaleState());
+    await updateTotalSupply();
+
+    addWalletListener();
+  };
+
+  prepare();
+}, []);
+
+const addWalletListener = () => {
+  if (window.ethereum) {
+    window.ethereum.on("accountsChanged", async (accounts) => {
+      if (accounts.length > 0) {
+        let balance = await web3.eth.getBalance(accounts[0]);
+        let formatedBalance = web3.utils.fromWei(balance, "ether");
+        let netWorkchainID = window.ethereum.networkVersion;
+        let chainName = chainMap[netWorkchainID].name;
+        setWalletChainName(chainName);
+        setWalletAddress(accounts[0]);
+        setWalletBalance(formatedBalance);
+        setStatus("");
+      } else {
+        setWalletAddress("");
+        setStatus("🦊 Connect to Metamask using Connect Wallet button.");
+      }
+    });
+
+    window.ethereum.on('chainChanged', async (chainId) => {      // bunun yerine networkChanged daha başarılı fakat kullanımdan kaldırılacak !
+      let networkId = window.ethereum.networkVersion;
+      let chainName = chainMap[networkId].name;
+      let accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+      let balance = await web3.eth.getBalance(accounts[0]);
+      let formatedBalance = web3.utils.fromWei(balance, "ether");
+      setWalletChainName(chainName);
+      setWalletAddress(accounts[0]);
+      setWalletBalance(formatedBalance);
+      window.location.reload();
+    });
+  }
+};
+
+  return (
+    <div className="App">
+     <main className="App-main">
+      <h3>
+        <span>MINT</span> NFT
+        </h3>
+        <img src={nftsGif} className="App-gif" alt=""/>
+
+        {isSaleActive ? (
+          <>
+          <button type="button" className="connect-button" onClick={connectWalletPressed}>
+          {walletAddress.length > 0 ? (
+                      "Connected: " + String(walletAddress).substring(0, 6) + "..." + String(walletAddress).substring(38)+
+                      "  | Balance : " + walletBalance +
+                      "  | Chain : " + walletChainName
+                    ) : (
+                      <span>Connect Wallet</span>
+                    )}
+          </button>
+
+          <p className="App-nftDetail">
+              <span className="App-mintAmount">{`${totalSupply}`}</span> /
+              10K
+          </p>
+
+          <div className="quantity">
+            <button className="quantity__minus" onClick={decrementCount}><span>-</span></button>
+              <h1 className="quantity__count">{count}</h1>
+            <button className="quantity__plus" onClick={incrementCount}><span>+</span></button>
+          </div>
+
+          <p className="App-mintGas">
+                {nftPrice} ETH{" "}
+                <span className="text-sm text-gray-300"> + GAS</span>
+          </p>
+
+          <button
+            className="connect-button bg-mint"
+            onClick={mintNFTPressed}
+          >
+            Mint now!
+          </button>
+        </>
+        ) : (
+          <p>
+            😥 Sale is not active yet!        
+          </p>
+        )}
+        <div className="App-status">    
+      {status && (     
+            <p>
+              {status}
+            </p>
+          )}
+          </div> 
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/interact.js
@@ -0,0 +1,178 @@
+import Web3 from "web3";
+import { chainMap } from "./chains";
+
+/*
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(process.env.NEXT_PUBLIC_API_URL);
+const contract = require("../artifacts/contracts/Aydogan.sol/Aydogan.json");
+const contractAddress = "0xd7C07aE2338bCf21868B23c7221AbA3a9Ee09383";
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);*/
+
+export const web3 = new Web3(Web3.givenProvider); 
+const nftWeb3 = new Web3(new Web3.providers.HttpProvider('https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'));
+const contract = require("./contracts/Aydogan.sol/Aydogan.json");
+const contractAddress = "0xd7C07aE2338bCf21868B23c7221AbA3a9Ee09383";
+export const nftContract = new nftWeb3.eth.Contract(contract.abi, contractAddress);
+
+export const connectWallet = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      const obj = {
+        status: "",
+        address: addressArray[0],
+      };
+
+      return obj;
+    } catch (err) {
+      return {
+        address: "",
+        status: "😞" + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://metamask.io/download.html"
+            >
+              You must install MetaMask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const getCurrentWalletConnected = async () => {
+  if (window.ethereum) {
+    try {
+      const addressArray = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      if (addressArray.length > 0) {
+        let balance = await web3.eth.getBalance(addressArray[0]);
+        let formatedBalance = web3.utils.fromWei(balance, "ether");
+        let netWorkchainID = window.ethereum.networkVersion;
+        let chainName = chainMap[netWorkchainID].name;
+        return {
+          address: addressArray[0],
+          status: "",
+          chainName: chainName,
+          balance: formatedBalance,
+        };
+      } else {
+        return {
+          address: "",
+          status: "😞",
+        };
+      }
+    } catch (err) {
+      return {
+        address: "",
+        status: "😞" + err.message,
+      };
+    }
+  } else {
+    return {
+      address: "",
+      status: (
+        <span>
+          <p>
+            {" "}
+            🦊{" "}
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://metamask.io/download.html"
+            >
+              You must install MetaMask, a virtual Ethereum wallet, in your
+              browser.
+            </a>
+          </p>
+        </span>
+      ),
+    };
+  }
+};
+
+export const changeChain = async () => {
+    await window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: web3.utils.toHex(5) }]
+    });
+};
+
+// Contract Methods
+
+export const getMaxMintAmount = async () => {
+  const result = await nftContract.methods.maxTokenPurchase().call();
+  return result;
+};
+
+export const getTotalSupply = async () => {
+  const result = await nftContract.methods.totalSupply().call();
+  return result;
+};
+
+export const getNftPrice = async () => {
+  const result = await nftContract.methods.tokenPrice().call();
+  const resultEther = web3.utils.fromWei(result, "ether");
+  return resultEther;
+};
+
+export const getSaleState = async () => {
+  const result = await nftContract.methods.presaleIsActive().call();
+  return result;
+};
+
+export const mintNFT = async (mintAmount) => {
+  await changeChain();
+  if (!window.ethereum.selectedAddress) {
+    return {
+      success: false,
+      status: ("🦊 Connect to Metamask using Connect Wallet button."),
+    };
+  }
+
+  //set up your Ethereum transaction
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: window.ethereum.selectedAddress, // must match user's active address.
+    value: parseInt(web3.utils.toWei("0.008", "ether") * mintAmount).toString(
+      16
+    ), // hex
+    gasLimit: "0",
+    data: nftContract.methods.mintAydogan(mintAmount).encodeABI(), //make call to NFT smart contract
+  };
+  //sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      success: true,
+      status:
+        "✅ Check out your transaction on Etherscan: https://rinkeby.etherscan.io/tx/" +
+        txHash,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message,
+    };
+  }
+};
